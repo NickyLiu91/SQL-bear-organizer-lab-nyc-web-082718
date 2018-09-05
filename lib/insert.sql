419e415579b4fbce65c57f0123501fb1ba5fd3d5
github username <github email address>
2018-09-05 12:56:19 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'male', 'red', 'violent', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'female', 'orange', 'very-violent', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'male', 'yellow', 'extremely-violent', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'female', 'green', 'scary-violent', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, 'male', 'blue', 'best-stay-away', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 6, 'female', 'indigo', 'you-asked-for-it', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, 'male', 'violet', 'absolutely-insane', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 9001, 'male', 'rainbow', 'actually-kind-of-nice', 1);
